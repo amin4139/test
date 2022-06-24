cc82687ea560fcbe505c0321b9763110b3207e41
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-06-24 18:20:02 +0800

    Update CFwarp.sh

--- CFwarp.sh
+++ CFwarp.sh
@@ -88,9 +88,9 @@ fi
 
 L6=$(ip route get 2606:4700:d0::a29f:c001 | grep -oP 'src \K\S+')
 L4=$(ip route get 162.159.193.1 | grep -oP 'src \K\S+')
-ud4='sed -i "5 s/^/PostUp = ip -4 rule add from $(ip route get 162.159.193.1 | grep -oP 'src \K\S+') lookup main\n/;6 s/^/PostDown = ip -4 rule delete from $(ip route get 162.159.193.1 | grep -oP 'src \K\S+') lookup main\n/" /etc/wireguard/wgcf.conf'
-ud6='sed -i "7 s/^/PostUp = ip -6 rule add from $(ip route get 2606:4700:d0::a29f:c001 | grep -oP 'src \K\S+') lookup main\n/;8 s/^/PostDown = ip -6 rule delete from $(ip route get 2606:4700:d0::a29f:c001 | grep -oP 'src \K\S+') lookup main\n/" /etc/wireguard/wgcf.conf'
-ud4ud6='sed -i "5 s/^/PostUp = ip -4 rule add from $(ip route get 162.159.193.1 | grep -oP 'src \K\S+') lookup main\n/;6 s/^/PostDown = ip -4 rule delete from $(ip route get 162.159.193.1 | grep -oP 'src \K\S+') lookup main\n/;7 s/^/PostUp = ip -6 rule add from $(ip route get 2606:4700:d0::a29f:c001 | grep -oP 'src \K\S+') lookup main\n/;8 s/^/PostDown = ip -6 rule delete from $(ip route get 2606:4700:d0::a29f:c001 | grep -oP 'src \K\S+') lookup main\n/" /etc/wireguard/wgcf.conf'
+ud4='sed -i "5 s/^/PostUp = ip -4 rule add from $L4 lookup main\n/;6 s/^/PostDown = ip -4 rule delete from $L4 lookup main\n/" /etc/wireguard/wgcf.conf'
+ud6='sed -i "7 s/^/PostUp = ip -6 rule add from $L6 lookup main\n/;8 s/^/PostDown = ip -6 rule delete from $L6 lookup main\n/" /etc/wireguard/wgcf.conf'
+ud4ud6='sed -i "5 s/^/PostUp = ip -4 rule add from $L4 lookup main\n/;6 s/^/PostDown = ip -4 rule delete from $L4 lookup main\n/;7 s/^/PostUp = ip -6 rule add from $L6 lookup main\n/;8 s/^/PostDown = ip -6 rule delete from $L6 lookup main\n/" /etc/wireguard/wgcf.conf'
 c1="sed -i '/0\.0\.0\.0\/0/d' /etc/wireguard/wgcf.conf"
 c2="sed -i '/\:\:\/0/d' /etc/wireguard/wgcf.conf"
 c3="sed -i 's/engage.cloudflareclient.com/162.159.193.10/g' /etc/wireguard/wgcf.conf"
