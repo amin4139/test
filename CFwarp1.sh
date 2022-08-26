884c2379d01e4e1441694e05dc135e5aa65047c7
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-08-26 02:12:24 +0000

    Update CFwarp1.sh

--- CFwarp1.sh
+++ CFwarp1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wpygV="22.8.25 V 0.1"
-remoteV=`wget -qO- https://gitlab.com/rwkgyg/CFwarp/raw/main/CFwarp.sh | sed -n 2p | cut -d '"' -f 2`
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export LANG=en_US.UTF-8
+wpygV="22.8.25 V 0.1"
+remoteV=`wget -qO- https://gitlab.com/rwkgyg/CFwarp/raw/main/CFwarp.sh | sed -n 4p | cut -d '"' -f 2`
 red='\033[0;31m'
 bblue='\033[0;34m'
 yellow='\033[0;33m'
