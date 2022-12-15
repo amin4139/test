84502b5b102eb119cc4db620232117dc523d5012
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-12-15 03:34:54 +0000

    Update 66.sh

--- 66.sh
+++ 66.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export LANG=en_US.UTF-8
-wpygV="22.12.15 V 3.0 "
+wpygV="22.99.99 V 3.0 "
 remoteV=`wget -qO- https://gitlab.com/rwkgyg/CFwarp/raw/main/66.sh | sed -n 4p | cut -d '"' -f 2`
 chmod +x /root/CFwarp.sh
 red='\033[0;31m'
