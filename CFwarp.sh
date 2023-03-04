06a6147894f32ce8551da061c7513a2e4cfb7fc4
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2023-03-04 08:19:42 +0000

    Update CFwarp.sh

--- CFwarp.sh
+++ CFwarp.sh
@@ -947,7 +947,7 @@ green "  0. 退出脚本 "
 red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 if [[ $(type -P warp-go) || $(type -P warp-cli) ]] && [[ -f '/root/CFwarp.sh' ]]; then
 if [ "${wpygV}" = "${remoteV}" ]; then
-echo -e " 当前 CFwarp 脚本版本号：${bblue}${wpygV}${plain} 重置版第三版 ，已是最新版本\n"
+echo -e " 当前 CFwarp 脚本版本号：${bblue}${wpygV}${plain} 重置版第四版 ，已是最新版本\n"
 else
 echo -e " 当前 CFwarp 脚本版本号：${bblue}${wpygV}${plain}"
 echo -e " 检测到最新 CFwarp 脚本版本号：${yellow}${remoteV}${plain}"
@@ -1527,9 +1527,9 @@ green "  0. 退出脚本 "
 red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 if [[ $(type -P wg-quick) || $(type -P warp-cli) ]] && [[ -f '/root/CFwarp.sh' ]]; then
 if [ "${wpygV}" = "${remoteV}" ]; then
-echo -e " 当前CFwarp脚本版本号：${bblue}${wpygV}${plain} 重置版第三版 ，已是最新版本\n"
+echo -e " 当前 CFwarp 脚本版本号：${bblue}${wpygV}${plain} 重置版第四版 ，已是最新版本\n"
 else
-echo -e " 当前CFwarp脚本版本号：${bblue}${wpygV}${plain}"
+echo -e " 当前 CFwarp 脚本版本号：${bblue}${wpygV}${plain}"
 echo -e " 检测到最新CFwarp脚本版本号：${yellow}${remoteV}${plain}"
 echo -e " ${yellow}$(wget -qO- https://gitlab.com/rwkgyg/CFwarp/raw/main/version/warpV)${plain}"
 echo -e " 可选择8进行更新\n"
