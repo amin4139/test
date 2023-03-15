9d8edde075089762130c9d59cf17cc891bd5d62e
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2023-03-15 06:39:07 +0000

    Update 661.sh

--- 661.sh
+++ 661.sh
@@ -1581,7 +1581,8 @@ fi
 cwg(){
 wg-quick down wgcf >/dev/null 2>&1
 systemctl disable wg-quick@wgcf >/dev/null 2>&1
-$yumapt autoremove wireguard-tools
+$yumapt remove wireguard-tools
+$yumapt autoremove
 dig9
 sed -i '/^precedence ::ffff:0:0\/96  100/d;/^label 2002::\/16   2/d' /etc/gai.conf
 rm -rf /usr/local/bin/wgcf /usr/bin/wg-quick /etc/wireguard/wgcf.conf /etc/wireguard/wgcf-profile.conf /etc/wireguard/buckup-account.toml /etc/wireguard/wgcf-account.toml /etc/wireguard/wgcf+p.log /etc/wireguard/ID /usr/bin/wireguard-go /usr/bin/wgcf wgcf-account.toml wgcf-profile.conf
