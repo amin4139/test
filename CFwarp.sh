a7a1d2bb63099d41ec9166d58602ce3e0fd9aa1b
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2023-05-07 00:33:08 +0000

    Update CFwarp.sh

--- CFwarp.sh
+++ CFwarp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export LANG=en_US.UTF-8
+wpygV="23.5.4 V 0.9.9 "
 [[ $EUID -ne 0 ]] && "请以root模式运行脚本" && exit
 if [[ -f /etc/redhat-release ]]; then
 release="centos"
